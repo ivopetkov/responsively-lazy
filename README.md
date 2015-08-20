2e94e2569717b2a2196a17a5873086da4ac2ac9f
Ivo Petkov <ivo@sylverstyle.com>
2015-08-21 00:55:26 +0300

    Support for images with unknown aspect ratio

--- README.md
+++ README.md
@@ -22,11 +22,14 @@ You can find a demo at [http://ivopetkov.github.io/responsivelyLazy/](http://ivo
     <img alt="" src="images/2500.jpg" data-srcset="images/600.jpg 600w, images/400.jpg 400w, images/800.jpg 800w, images/1500.jpg 1500w, images/1000.jpg 1000w, images/2000.jpg 2000w" srcset="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" />
 </div>
 ```
-The things to customize are the **padding-bottom** style, and the values of the **src** and **data-srcset** attributes.
+The things to customize are the **padding-bottom** style, and the values of the **src** and **data-srcset** attributes. If you don't know the image aspect ratio you can skip the div tag and move the responsively-lazy class to te img tag:
+```
+<img class="responsively-lazy" alt="" src="images/2500.jpg" data-srcset="images/600.jpg 600w, images/400.jpg 400w, images/800.jpg 800w, images/1500.jpg 1500w, images/1000.jpg 1000w, images/2000.jpg 2000w" srcset="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" />
+```
 
 ## Browser support
 
 The lazy loading works in browsers supporting the srcset attribute. As of August 2015 that's [62.78%](http://caniuse.com/#feat=srcset). Unsupported browsers will load the image in the src attribute.
 
 ## Got questions?
-You can find me at [@IvoPetkovCom](https://twitter.com/IvoPetkovCom) or [ivopetkov.com](http://ivopetkov.com)
+You can find me at [@IvoPetkovCom](https://twitter.com/IvoPetkovCom) and [ivopetkov.com](http://ivopetkov.com)
--- responsivelyLazy.js
+++ responsivelyLazy.js
@@ -22,61 +22,69 @@ if (typeof responsivelyLazy === 'undefined') {
 
     responsivelyLazy.run = function () {
 
-        var elements = document.querySelectorAll('.responsively-lazy > img');
-        var elementsCount = elements.length;
-        for (var i = 0; i < elementsCount; i++) {
-            var element = elements[i];
-            var container = element.parentNode;
-            if (!responsivelyLazy.isVisible(container)) {
-                continue;
-            }
-            var options = element.getAttribute('data-srcset');
-            if (options !== null) {
-                options = options.split(',');
-                var temp = [];
+        var update = function (elements, unknownHeight) {
+            var elementsCount = elements.length;
+            for (var i = 0; i < elementsCount; i++) {
+                var element = elements[i];
+                var container = unknownHeight ? element : element.parentNode;
+                if (!responsivelyLazy.isVisible(container)) {
+                    continue;
+                }
+                var options = element.getAttribute('data-srcset');
+                if (options !== null) {
+                    options = options.split(',');
+                    var temp = [];
+                    var optionsCount = options.length;
+                    for (var j = 0; j < optionsCount; j++) {
+                        var option = options[j].trim();
+                        var spaceIndex = option.lastIndexOf(' ');
+                        temp.push([option.substr(0, spaceIndex), parseInt(option.substr(spaceIndex + 1, option.length - spaceIndex - 2), 10)]);
+                    }
+                    temp.sort(function (a, b) {
+                        if (a[1] < b[1]) {
+                            return -1;
+                        }
+                        if (a[1] > b[1]) {
+                            return 1;
+                        }
+                        return 0;
+                    });
+                    options = temp;
+                } else {
+                    options = [];
+                }
+                var containerWidth = container.offsetWidth * window.devicePixelRatio;
+
+                var bestSelectedOption = null;
                 var optionsCount = options.length;
                 for (var j = 0; j < optionsCount; j++) {
-                    var option = options[j].trim();
-                    var spaceIndex = option.lastIndexOf(' ');
-                    temp.push([option.substr(0, spaceIndex), parseInt(option.substr(spaceIndex + 1, option.length - spaceIndex - 2), 10)]);
-                }
-                temp.sort(function (a, b) {
-                    if (a[1] < b[1]) {
-                        return -1;
+                    var optionData = options[j];
+                    if (optionData[1] >= containerWidth) {
+                        bestSelectedOption = optionData;
+                        break;
                     }
-                    if (a[1] > b[1]) {
-                        return 1;
-                    }
-                    return 0;
-                });
-                options = temp;
-            } else {
-                options = [];
-            }
-            var containerWidth = container.offsetWidth * window.devicePixelRatio;
-
-            var bestSelectedOption = null;
-            var optionsCount = options.length;
-            for (var j = 0; j < optionsCount; j++) {
-                var optionData = options[j];
-                if (optionData[1] >= containerWidth) {
-                    bestSelectedOption = optionData;
-                    break;
                 }
-            }
-            if (bestSelectedOption === null) {
-                bestSelectedOption = [element.getAttribute('src'), 999999];
-            }
+                if (bestSelectedOption === null) {
+                    bestSelectedOption = [element.getAttribute('src'), 999999];
+                }
 
-            if (typeof container.lastSetOption === 'undefined') {
-                container.lastSetOption = ['', 0];
-            }
-            if (container.lastSetOption[1] < bestSelectedOption[1]) {
-                container.lastSetOption = bestSelectedOption;
-                element.setAttribute('srcset', bestSelectedOption[0]);
-            }
+                if (typeof container.lastSetOption === 'undefined') {
+                    container.lastSetOption = ['', 0];
+                }
+                if (container.lastSetOption[1] < bestSelectedOption[1]) {
+                    container.lastSetOption = bestSelectedOption;
+                    element.setAttribute('srcset', bestSelectedOption[0]);
+                    if (unknownHeight) {
+                        element.style.height = "auto";
+                    }
+                }
 
-        }
+            }
+        };
+
+        update(document.querySelectorAll('.responsively-lazy > img'), false);
+        update(document.querySelectorAll('img.responsively-lazy'), true);
+
     };
 
     if ('srcset' in document.createElement('img') && typeof window.devicePixelRatio !== 'undefined' && typeof window.addEventListener !== 'undefined' && typeof document.querySelectorAll !== 'undefined') {
